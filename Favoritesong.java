ede8844a8935418959d105c31ef3da1ad0460067
ChristopherJohnEntice-UCMN <johnjohnenticelove6@gmail.com>
2025-08-29 22:15:03 +0800

    created a file

---
+++ Favoritesong.java
@@ -0,0 +1,8 @@
+public class Favoritesong {
+    public static void main(String[] args){
+        System.out.println("All I dream of is your eyes");
+        System.out.println("All I long for is your touch");
+        System.out.println("And, darling, something tells me, tells me it's enough");
+        System.out.println("You can say that I'm a fool And I don't know very much But I think they call this love");
+    }
+}
---
+++ TableandChairs.java
@@ -0,0 +1,12 @@
+
+
+public class TableandChairs {
+    public static void main(String[] args) {
+        System.out.println("X                       X");
+        System.out.println("X                       X");
+        System.out.println("X                       X");
+        System.out.println("XXXXX  XXXXXXXXXXX  XXXXX");
+        System.out.println("X   X  X         X  X   X");
+        System.out.println("X   X  X         X  X   X");
+    }
+}
