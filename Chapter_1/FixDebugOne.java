dddd0d0c391428cadefa9d688acdf8ea33b721db
ChristopherJohnEntice-UCMN <johnjohnenticelove6@gmail.com>
2025-09-01 15:21:53 +0800

    FixDebug Assignment

---
+++ Chapter_1/FixDebugOne.java
@@ -0,0 +1,22 @@
+public class FixDebugOne
+{
+   /* This program displays a greeting */
+   public static void main(String[] args)   
+   {
+      System.out.println("Hello World!");
+   }
+}
+/*
+List down the spotted bugs below:
+
+e.g.
+- // wrong syntax for access modifier "publlic" instead of "public"
+
+Bugs:
+1. public class name has a space on it
+2. The Main is Capitalize
+3. String args should be String [] args
+4. Systm is spelled wrong it should be System
+5. Missing Semi Colon
+6. No end } braces
+*/
