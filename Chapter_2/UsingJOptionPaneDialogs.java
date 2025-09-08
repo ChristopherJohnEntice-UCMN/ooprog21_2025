aee6e609613ca2642efbc504cf269d3c611c2549
ChristopherJohnEntice-UCMN <johnjohnenticelove6@gmail.com>
2025-09-08 22:37:11 +0800

    Updated JOptionPane code

--- Chapter_2/UsingJOptionPaneDialogs.java
+++ Chapter_2/UsingJOptionPaneDialogs.java
@@ -4,15 +4,21 @@ public class UsingJOptionPaneDialogs {
 
     public static void main(String[] a) {
         String nameValue;
+        String sectionValue;
         int confirmationValue;
+        
+
+        JOptionPane.showMessageDialog(null, "Welcome to Ooprog Quiz 1.01");
 
         do{
+            
             nameValue = JOptionPane.showInputDialog(null, "What is your name?");
+            sectionValue = JOptionPane.showInputDialog(null, "What is your Course and Section?");
             confirmationValue = JOptionPane.showConfirmDialog(null,"would you like to confirm?");
         }while(confirmationValue == 1);
 
 
-        JOptionPane.showMessageDialog(null, "My Name is: " + nameValue);
+        JOptionPane.showMessageDialog(null, "My Name is: " + nameValue + " Section: " + sectionValue);
 
     }
     
